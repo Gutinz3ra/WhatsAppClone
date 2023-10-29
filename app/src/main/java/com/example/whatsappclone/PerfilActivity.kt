409b31e21610dcd7e331d13d02954ec1fe15273c
gutinz3ra <augustomzemor@gmail.com>
2023-10-29 15:30:54 +0100

    Implementações na interface de perfil...

--- app/src/main/java/com/example/whatsappclone/PerfilActivity.kt
+++ app/src/main/java/com/example/whatsappclone/PerfilActivity.kt
@@ -1,11 +1,31 @@
 package com.example.whatsappclone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whatsappclone.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
+
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate(layoutInflater)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView(binding.root)
+        inicializarToolbar()
+
     }
+
+    private fun inicializarToolbar() {
+        val toolbar = binding.includeToolbarPerfil.tbPrincipal
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            title = "Editar Perfil"
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
 }
