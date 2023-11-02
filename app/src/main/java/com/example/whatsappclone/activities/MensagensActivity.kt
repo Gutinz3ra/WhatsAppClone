c4148c89788568871d44fd724531822fd72a3ac4
gutinz3ra <augustomzemor@gmail.com>
2023-11-02 18:25:36 +0100

    .

--- app/src/main/java/com/example/whatsappclone/activities/MensagensActivity.kt
+++ app/src/main/java/com/example/whatsappclone/activities/MensagensActivity.kt
@@ -2,59 +2,242 @@ package com.example.whatsappclone.activities
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.whatsappclone.adapters.ConversasAdapter
 import com.example.whatsappclone.databinding.ActivityMensagensBinding
+import com.example.whatsappclone.model.Conversa
+import com.example.whatsappclone.model.Mensagem
 import com.example.whatsappclone.model.Usuario
 import com.example.whatsappclone.utils.Constantes
+import com.example.whatsappclone.utils.exibirmensagem
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
 import com.squareup.picasso.Picasso
 
 class MensagensActivity : AppCompatActivity() {
 
-    private val binding by lazy {
-        ActivityMensagensBinding.inflate(layoutInflater)
+    private val firebaseAuth by lazy {
+        FirebaseAuth.getInstance()
     }
+    private val firestore by lazy {
+        FirebaseFirestore.getInstance()
+    }
+
+    private val binding by lazy {
+        ActivityMensagensBinding.inflate( layoutInflater )
+    }
+    private lateinit var listenerRegistration: ListenerRegistration
     private var dadosDestinatario: Usuario? = null
+    private var dadosUsuarioRementente: Usuario? = null
+    private lateinit var conversasAdapter: ConversasAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        recuperarDadosUsuarioDestinatario()
+        setContentView( binding.root )
+        recuperarDadosUsuarios()
         inicializarToolbar()
+        inicializarEventoClique()
+        inicializarRecyclerview()
+        inicializarListeners()
+    }
 
+    private fun inicializarRecyclerview() {
+
+        with(binding){
+            conversasAdapter = ConversasAdapter()
+            rvMensagens.adapter = conversasAdapter
+            rvMensagens.layoutManager = LinearLayoutManager(applicationContext)
+        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        listenerRegistration.remove()
+    }
+
+    private fun inicializarListeners() {
+
+        val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+        val idUsuarioDestinatario = dadosDestinatario?.id
+        if( idUsuarioRemetente != null && idUsuarioDestinatario != null ){
+
+            listenerRegistration = firestore
+                .collection(Constantes.MENSAGENS)
+                .document( idUsuarioRemetente )
+                .collection( idUsuarioDestinatario )
+                .orderBy("data", Query.Direction.ASCENDING)
+                .addSnapshotListener { querySnapshot, erro ->
+
+                    if( erro != null ){
+                        exibirmensagem("Erro ao recuperar mensagens")
+                    }
+
+                    val listaMensagens = mutableListOf<Mensagem>()
+                    val documentos = querySnapshot?.documents
+
+                    documentos?.forEach { documentSnapshot ->
+                        val mensagem = documentSnapshot.toObject( Mensagem::class.java )
+                        if( mensagem != null ){
+                            listaMensagens.add( mensagem )
+                            Log.i("exibicao_mensagens", mensagem.mensagem)
+                        }
+                    }
+
+                    //Lista
+                    if( listaMensagens.isNotEmpty() ){
+                        //Carregar os dados Adapter
+                        conversasAdapter.adicionarLista( listaMensagens )
+                    }
+
+                }
+
+        }
+
+    }
+
+    private fun inicializarEventoClique() {
+
+        binding.fabEnviar.setOnClickListener {
+            val mensagem = binding.editMensagem.text.toString()
+            salvarMensagem( mensagem )
+        }
+
+    }
+
+    private fun salvarMensagem( textoMensagem: String ) {
+        if( textoMensagem.isNotEmpty() ){
+
+            val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+            val idUsuarioDestinatario = dadosDestinatario?.id
+            if( idUsuarioRemetente != null && idUsuarioDestinatario != null ){
+                val mensagem = Mensagem(
+                    idUsuarioRemetente, textoMensagem
+                )
+
+                //Salvar para o Remetente
+                salvarMensagemFirestore(
+                    idUsuarioRemetente, idUsuarioDestinatario, mensagem
+                )
+                //Jamilton -> Foto e nome Destinatario (ana)
+                val conversaRemetente = Conversa(
+                    idUsuarioRemetente, idUsuarioDestinatario,
+                    dadosDestinatario!!.foto, dadosDestinatario!!.nome,
+                    textoMensagem
+                )
+                salvarConversaFirestore( conversaRemetente )
+
+                //Salvar mesma mensagem para o destinatario
+                salvarMensagemFirestore(
+                    idUsuarioDestinatario, idUsuarioRemetente, mensagem
+                )
+                //Ana -> Foto e nome Remente (jamilton)
+                val conversaDestinatario = Conversa(
+                    idUsuarioDestinatario, idUsuarioRemetente,
+                    dadosUsuarioRementente!!.foto, dadosUsuarioRementente!!.nome,
+                    textoMensagem
+                )
+                salvarConversaFirestore( conversaDestinatario )
+
+                binding.editMensagem.setText("")
+
+            }
+
+        }
+    }
+
+    private fun salvarConversaFirestore(conversa: Conversa) {
+        firestore
+            .collection(Constantes.CONVERSAS)
+            .document( conversa.idUsuarioRementente )
+            .collection(Constantes.ULTIMAS_CONVERSAS)
+            .document( conversa.idUsuarioDestinatario )
+            .set( conversa )
+            .addOnFailureListener {
+                exibirmensagem("Erro ao salvar conversa")
+            }
+
+    }
+
+    private fun salvarMensagemFirestore(
+        idUsuarioRemetente: String,
+        idUsuarioDestinatario: String,
+        mensagem: Mensagem
+    ) {
+
+        firestore
+            .collection(Constantes.MENSAGENS)
+            .document( idUsuarioRemetente )
+            .collection( idUsuarioDestinatario )
+            .add( mensagem )
+            .addOnFailureListener {
+                exibirmensagem("Erro ao enviar mensagem")
+            }
 
     }
 
     private fun inicializarToolbar() {
         val toolbar = binding.tbMensagens
-        setSupportActionBar(toolbar)
+        setSupportActionBar( toolbar )
         supportActionBar?.apply {
             title = ""
-            if (dadosDestinatario != null){
+            if( dadosDestinatario != null ){
                 binding.textNome.text = dadosDestinatario!!.nome
                 Picasso.get()
                     .load(dadosDestinatario!!.foto)
-                    .into(binding.ImageFotoPerfil)
+                    .into( binding.imageFotoPerfil )
             }
             setDisplayHomeAsUpEnabled(true)
         }
     }
 
-    private fun recuperarDadosUsuarioDestinatario() {
+    private fun recuperarDadosUsuarios() {
+
+        //Dados do usuário logado
+        val idUsuarioRemetente = firebaseAuth.currentUser?.uid
+        if( idUsuarioRemetente != null ){
+            firestore
+                .collection(Constantes.USUARIOS)
+                .document( idUsuarioRemetente )
+                .get()
+                .addOnSuccessListener { documentSnapshot ->
+
+                    val usuario = documentSnapshot.toObject(Usuario::class.java)
+                    if( usuario != null ){
+                        dadosUsuarioRementente = usuario
+                    }
+
+                }
+        }
+
+        //Recuperando dados destinatário
         val extras = intent.extras
-        if (extras != null){
+        if( extras != null ){
+
             val origem = extras.getString("origem")
-            if (origem == Constantes.ORIGEM_CONTATO){
+            if( origem == Constantes.ORIGEM_CONTATO ){
 
                 dadosDestinatario = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                    extras.getParcelable("dadosDestinatario",Usuario::class.java)
+                    extras.getParcelable(
+                        "dadosDestinatario",
+                        Usuario::class.java
+                    )
                 }else{
-                    extras.getParcelable("dadosDestinatario")
+                    extras.getParcelable(
+                        "dadosDestinatario"
+                    )
                 }
-            }else if(origem == Constantes.ORIGEM_CONVERSA){
-                //recuperar dados conversa
 
+            }else if( origem == Constantes.ORIGEM_CONVERSA ){
+                //Recuperar os dados da conversa
 
             }
+
         }
+
     }
 }
---
+++ app/src/main/java/com/example/whatsappclone/adapters/ConversasAdapter.kt
@@ -0,0 +1,106 @@
+package com.example.whatsappclone.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.google.firebase.auth.FirebaseAuth
+import com.example.whatsappclone.databinding.ItemContatosBinding
+import com.example.whatsappclone.databinding.ItemMensagensDestinatarioBinding
+import com.example.whatsappclone.databinding.ItemMensagensRemetenteBinding
+import com.example.whatsappclone.model.Mensagem
+import com.example.whatsappclone.model.Usuario
+import com.example.whatsappclone.utils.Constantes
+
+class ConversasAdapter : Adapter<ViewHolder>() {
+
+    private var listaMensagens = emptyList<Mensagem>()
+    fun adicionarLista( lista: List<Mensagem> ){
+        listaMensagens = lista
+        notifyDataSetChanged()
+    }
+
+    class MensagensRemetenteViewHolder(//ViewHolder
+        private val binding: ItemMensagensRemetenteBinding
+    ) : ViewHolder( binding.root ){
+
+        fun bind( mensagem: Mensagem ){
+            binding.textMensagemRemetente.text = mensagem.mensagem
+        }
+
+        companion object {
+            fun inflarLayout( parent: ViewGroup ) : MensagensRemetenteViewHolder {
+
+                val inflater = LayoutInflater.from( parent.context )
+                val itemView = ItemMensagensRemetenteBinding.inflate(
+                    inflater, parent, false
+                )
+                return MensagensRemetenteViewHolder( itemView )
+
+            }
+        }
+
+    }
+
+    class MensagensDestinatarioViewHolder(//ViewHolder
+        private val binding: ItemMensagensDestinatarioBinding
+    ) : ViewHolder( binding.root ){
+
+        fun bind( mensagem: Mensagem ){
+            binding.textMensagemDestinatario.text = mensagem.mensagem
+        }
+
+        companion object {
+            fun inflarLayout( parent: ViewGroup ) : MensagensDestinatarioViewHolder {
+
+                val inflater = LayoutInflater.from( parent.context )
+                val itemView = ItemMensagensDestinatarioBinding.inflate(
+                    inflater, parent, false
+                )
+                return MensagensDestinatarioViewHolder( itemView )
+
+            }
+        }
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        val mensagem = listaMensagens[position]
+        val idUsuarioLogado = FirebaseAuth.getInstance().currentUser?.uid.toString()
+
+        return if( idUsuarioLogado == mensagem.idUsuario ){
+            Constantes.TIPO_REMETENTE
+        }else{
+            Constantes.TIPO_DESTINATARIO
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        if( viewType == Constantes.TIPO_REMETENTE )
+            return MensagensRemetenteViewHolder.inflarLayout( parent )
+
+        return MensagensDestinatarioViewHolder.inflarLayout( parent )
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val mensagem = listaMensagens[position]
+        when( holder ){
+            is MensagensRemetenteViewHolder -> holder.bind(mensagem)
+            is MensagensDestinatarioViewHolder -> holder.bind(mensagem)
+        }
+        /*val mensagensRemetenteViewHolder = holder as MensagensRemetenteViewHolder
+        mensagensRemetenteViewHolder.bind()*/
+
+    }
+
+    override fun getItemCount(): Int {
+        return listaMensagens.size
+    }
+
+}
